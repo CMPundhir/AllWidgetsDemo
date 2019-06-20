fd9a80055ce8e83863a25f3c5cab241f89618bd8
CMPundhir <g.pundhir8802805015@gmail.com>
2019-06-20 12:36:26 +0530

    ButtonActivity added

---
+++ app/src/main/java/com/cmpundhir/cm/allwidgetsdemo/ButtonActivity.java
@@ -0,0 +1,14 @@
+package com.cmpundhir.cm.allwidgetsdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button);
+    }
+}
